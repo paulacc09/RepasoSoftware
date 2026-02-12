d12e2d166a22ec1c1358d9d88836cf6c8dc904e6
paulacc09 <paulacostacc11@gmail.com>
2026-02-12 17:11:28 -0500

    Biblioteca en main

--- src/main/java/org/yourcompany/yourproject/Main.java
+++ src/main/java/org/yourcompany/yourproject/Main.java
@@ -7,11 +7,15 @@ package org.yourcompany.yourproject;
 
 public class Main{
 public static void main(String[] args) {
+
+    Biblioteca biblioteca = new Biblioteca();
     Bibliotecario bibliotecario1 = new Bibliotecario("123", "Juan", "juan@biblioteca.com", "Mañana");
     System.out.println(bibliotecario1.darEmail());
 
     Socio socio1 = new Socio("456", "Maria", "maria@biblioteca.com", 5);
     socio1.registrarPrestamo(2);
+    biblioteca.agregarSocio(socio1);
+    System.err.println(biblioteca.darSocios().get(0).darNombre());
 
     
 
