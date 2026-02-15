060ba108d93b4404942db141bdb88bc5b1035fda
MichelleDiaz06 <87913882+MichelleDiaz06@users.noreply.github.com>
2026-02-15 11:44:26 -0500

    Libro

---
+++ src/main/java/org/yourcompany/yourproject/Libro.java
@@ -0,0 +1,24 @@
+package org.yourcompany.yourproject;
+
+import java.util.ArrayList;
+
+public class Libro {
+
+    private String isbn;
+    private String titulo;
+    private ArrayList<Ejemplar> ejemplares = new ArrayList<>();
+
+    public Libro(String titulo, String isbn){
+        this.titulo = titulo;
+        this.isbn = isbn;
+    }  
+    
+    public void crearEjemplar(String isbn) {
+        Ejemplar nuevo = new Ejemplar(isbn); 
+        ejemplares.add(nuevo);
+    }
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+}
